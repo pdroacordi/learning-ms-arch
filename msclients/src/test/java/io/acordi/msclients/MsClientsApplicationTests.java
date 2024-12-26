6a0b8a89be51b2a72d3fc0b13c0c19c63c8350c1
Pedro Acordi <pedroacordi2@gmail.com>
2024-12-26 13:26:42 -0300

    feat: setting up client microservice

---
+++ msclients/src/test/java/io/acordi/msclients/MsClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.acordi.msclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
